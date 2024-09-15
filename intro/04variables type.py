b08ac00939b28156193b3f784425d4bd2a05936c
anish <galacticoanish@gmail.com>
2024-09-15 09:19:40 +0545

    py

---
+++ intro/04variables type.py
@@ -0,0 +1,47 @@
+# Create a variable half
+half=0.5
+
+# Create a variable intro
+intro="hello! How are you?"
+
+# Create a variable is_good
+is_good = True
+'''isdusdu'''
+# In [1]:
+# type(a)
+# Out[1]:
+# float
+# In [2]:
+# type(b)
+# Out[2]:
+# str
+monthly_savings = 10
+num_months = 12
+intro = "Hello! How are you?"
+
+# Calculate year_savings using monthly_savings and num_months
+year_savings=monthly_savings*num_months;
+
+# Print the type of year_savings
+print(type(year_savings))
+
+# Assign sum of intro and intro to doubleintro
+doubleintro=intro+intro
+
+# Print out doubleintro
+print(doubleintro)
+
+
+
+# Definition of savings and total_savings
+savings = 100
+total_savings = 150
+
+# Fix the printout
+print("I started with $" + str(savings) + " and now have $" + str(total_savings) + ". Awesome!")
+
+# Definition of pi_string
+pi_string = "3.1415926"
+
+# Convert pi_string into float: pi_float
+pi_float = float(pi_string)
---
+++ intro/variablesandtypes.py
@@ -0,0 +1,8 @@
+#specific,case-sensitive name
+#call up value through variable name
+#type(variable)
+# Create a variable savings
+savings=100;
+
+# Print out savings
+print(savings)
